3c371f09b21e2caf8d1b7cfb8b85261295b5e441
hellcoldby <coldbao@gmail.com>
2017-02-08 23:36:22 +0800

    平均值

---
+++ 9.求平均值/9.求平均值.js
@@ -0,0 +1,16 @@
+var arr = [1,2,3]
+
+
+function average(){
+    var arr = [];
+    for(var i=0; i<arguments.length; i++){
+        arr.push(arguments[i]);
+    }
+
+    arr.sort(function(a,b){return a-b});
+    //去掉一个最大值，去掉一个最小值
+    arr.shift();
+    arr.pop();
+    return eval(arr.join('+'))arr.length; 
+}
+
